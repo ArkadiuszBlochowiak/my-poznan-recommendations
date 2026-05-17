80452b05268bd22d2fd103bbb78292623e87f24e
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-05-17 23:05:23 +0200

    Fix theme status bar

--- app/src/main/java/com/example/mypoznan/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mypoznan/ui/theme/Theme.kt
@@ -260,15 +260,6 @@ fun MyPoznanTheme(
         else -> lightScheme
     }
 
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = AppTypography,
